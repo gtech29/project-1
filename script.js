42e66587bae32dd1ddd73d98e91237e29fe04a63
gtech29 <juan.rodriguez.517@my.csun.edu>
2026-02-12 16:30:28 -0800

    Update navigation and titles to reflect featured albums; modify fetch URL in script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const postsContainer = document.getElementById("posts");
 
 // Use the Fetch API to get data from the JSONPlaceholder API
-fetch("https://www.theaudiodb.com/api/v1/json/123/searchalbum.php?s=The+Doors")
+fetch("#")
   .then((response) => response.json()) // Convert the response to JSON format
   .then((data) => {
     const albumsArray = data.album; // access the array inside the object
@@ -21,3 +21,4 @@ fetch("https://www.theaudiodb.com/api/v1/json/123/searchalbum.php?s=The+Doors")
     });
   })
   .catch((error) => console.error("Error fetching posts:", error));
+//https://www.theaudiodb.com/api/v1/json/123/searchalbum.php?s=The+Doors
