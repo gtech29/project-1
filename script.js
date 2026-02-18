91ee4001aa08f5ab6cb410f7d9f69987389c37c1
gtech29 <juan.rodriguez.517@my.csun.edu>
2026-02-17 18:08:57 -0800

    Refactor album fetching logic to handle empty data and improve error handling

--- script.js
+++ script.js
@@ -7,18 +7,28 @@ fetch("https://www.theaudiodb.com/api/v1/json/123/searchalbum.php?s=The+Doors")
   .then((data) => {
     const albumsArray = data.album; // access the array inside the object
 
-    // Loop through each post and display it on the page
-    albumsArray.forEach((album) => {
-      // Create a new div element for each post
-      const postDiv = document.createElement("div");
-      postDiv.innerHTML = `
+    
+    if (Array.isArray(albumsArray) && albumsArray.length > 0) {
+      // Loop through each post and display it on the page
+      albumsArray.forEach((album) => {
+        // Create a new div element for each post
+        const postDiv = document.createElement("div");
+        postDiv.innerHTML = `
         <h2>${album.strAlbum}</h2>
         <img src ="${album.strAlbumThumb}" >
         <p>${album.strDescriptionEN}</p>
       `;
-      // Append the new div to the container
-      postsContainer.appendChild(postDiv);
-    });
+        // Append the div to the container
+        postsContainer.appendChild(postDiv);
+      });
+    } else {
+      const errorDiv = document.createElement("div");
+      errorDiv.innerHTML = `<h2>No Data Available</h2>`;
+
+      // Append the div to the container
+      postsContainer.append(errorDiv);
+    }
+
   })
   .catch((error) => console.error("Error fetching posts:", error));
 
